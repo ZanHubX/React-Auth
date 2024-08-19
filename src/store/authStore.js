a793c995cc20f57389fd5ae117b067409bde6b76
hosh1dEcho <kaungzanthaw27904@gmail.com>
2024-08-19 17:26:01 +0630

    del

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -1,11 +1,1 @@
-import { create } from "zustand";
-
-// Create a Zustand store
-const authStore = create((set) => ({
-  isAuthenticated: false,
-  login: () => set({ isAuthenticated: true }),
-  logout: () => set({ isAuthenticated: false }),
-}));
-
-export default authStore;
 
