86f12344644ace1de86b24c04dad33fd56b5a3f3
hosh1dEcho <kaungzanthaw27904@gmail.com>
2024-08-19 17:27:21 +0630

    commit

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,19 +1,19 @@
 
-import React from "react";
-import ReactDOM from "react-dom";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import LoginPage from "./LoginPage";
-import SignupPage from "./SignupPage";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import LoginPage from "./LoginPage";
+// import SignupPage from "./SignupPage";
 
-const router = createBrowserRouter([
-  {
-    path: "/login",
-    element: <LoginPage />,
-  },
-  {
-    path: "/signup",
-    element: <SignupPage />,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/login",
+//     element: <LoginPage />,
+//   },
+//   {
+//     path: "/signup",
+//     element: <SignupPage />,
+//   },
+// ]);
 
-export default router;
+// export default router;
